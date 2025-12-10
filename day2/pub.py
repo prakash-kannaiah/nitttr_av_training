ffd9f4e24e6479c4bc69e3b82bd61d46c93f82d5
Prakash Kannaiah <105844116+prakash-kannaiah@users.noreply.github.com>
2025-12-10 15:57:15 +0530

    Rename class and update publisher details

--- day2/pub.py
+++ day2/pub.py
@@ -2,24 +2,24 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
-class frst_cls(Node):
-	def __init__(self):
-		super().__init__('frst_node')
-		self.publisher = self.create_publisher(String, 'myTopic', 10)
-		self.calFn = self.create_timer(1.0,self.myFn)		
+class Talker(Node):
+    def __init__(self):
+        super().__init__('talker')
+        self.publisher = self.create_publisher(String, 'chatter', 10)
+        self.timer = self.create_timer(1.0, self.timer_callback)
 
-	def myFn(self):
-		msg = String()
-		msg.data = 'hello from ROS'
-		self.publisher.publish(msg)
-		self.get_logger().info(msg.data)
-		
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello ROS 2'
+        self.publisher.publish(msg)
+        self.get_logger().info(f'Publishing: "{msg.data}"')
+
 def main(args=None):
-	rclpy.init(args=args)
-	myObj = frst_cls()
-	rclpy.spin(myObj)
-	myObj.destroy_node()
-	rclpy.shutdown()	
+    rclpy.init(args=args)
+    node = Talker()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
-	main()
+    main()
